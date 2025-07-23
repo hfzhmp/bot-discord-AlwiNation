8942018c6805839faba9f2e31c6293c9537661b4
Hafizh <cx41050@gmail.com>
2025-07-24 00:10:47 +0700

    Add Feature: Real Time Player Count Display

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,4 +1,8 @@
 const { Events } = require('discord.js');
+const mc = require('node-mcstatus');
+
+const serverAddress = 'play.alwination.id';
+const offlineStatus = 'Jangan Xray~';
 
 module.exports = {
   name: Events.ClientReady,
@@ -7,5 +11,24 @@ module.exports = {
   async execute(client) {
     console.log(`Bot telah login sebagai ${client.user.tag}!`);
 	  console.log(`Waktu aktif: ${new Date().toLocaleString('id-ID', { timeZone: 'Asia/Jakarta' })}`);
+
+    const updatePlayerCountActivity = async () => {
+      try {
+        const response = await mc.statusJava(serverAddress); 
+    
+        if (response.online) {
+          const playerCount = response.players.online;
+          client.user.setActivity(`with ${playerCount} players`, { type: ActivityType.Playing });
+        } else {
+          client.user.setActivity(offlineStatus, { type: ActivityType.Playing });
+        }
+      } catch (error) {
+        console.error(`Error saat query status server: ${error.message}`);
+        client.user.setActivity(offlineStatus, { type: ActivityType.Playing });
+      }
+    };
+
+    await updatePlayerCountActivity();
+    setInterval(updatePlayerCountActivity, 300000);
   },
 };
